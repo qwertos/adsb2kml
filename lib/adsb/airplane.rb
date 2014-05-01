fe0af061039e71edbd191f7b876eff009e25eb7d
Aaron Herting <aaron@herting.cc>
2014-05-01 12:31:51 -0400

    added airplane merge

--- lib/adsb/airplane.rb
+++ lib/adsb/airplane.rb
@@ -3,12 +3,39 @@
 module ADSB
 	class Airplane
 		
-		attr_accessor :latitude, :longitude, :last_heard, :speed, :vert_rate, :track
+		@@info = [
+			:latitude,
+			:longitude,
+			:last_heard,
+			:speed,
+			:vert_rate,
+			:track
+		]
+
+		@@info.each do |x|
+			attr_accessor x
+		end
+
 		attr_reader :address
 
 		def initialize address
 			@address = address
 		end
+
+		def merge airplane
+			if airplane.address != @address then
+				return false
+			end
+			
+			@@info.each do |x|
+				if ! airplane.send(x).empty? then
+					send( x.to_s + "=" , airplane.send(x) )
+				end
+			end
+
+			return true
+		end
+			
 	end
 end
 
