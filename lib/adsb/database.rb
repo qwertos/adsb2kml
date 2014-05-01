6ff46aa78f5b1d3b169360b2f3ca9a7e59b16428
Aaron Herting <aaron@herting.cc>
2014-05-01 12:24:33 -0400

    should be creating the KML now

---
+++ lib/adsb/database.rb
@@ -0,0 +1,40 @@
+
+require 'ruby_kml'
+
+module ADSB
+	class Database
+		
+		def initialize name = "ADSB"
+			@name = name
+			@airplanes = {}
+		end
+
+
+		def update airplane
+			if ( @airplanes.has_key? airplane.address ) then
+				@airplanes[airplane.address].merge_with airplane
+			else
+				@airplanes[airplane.address] = airplane
+			end
+		end
+
+
+		def to_kml
+			kml = KMLFile.new
+			folder = KML::Folder.new( :name => @name )
+			
+			@airplanes.each do |address, airplane|
+				folder.features << airplane.placemark
+			end
+
+			kml.objects << folder
+
+			return kml
+		end
+
+		def to_s
+			return "TODO"
+		end
+	end
+end
+
