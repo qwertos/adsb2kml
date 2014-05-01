3172e235b5f4672af40cf7971663421f38d298ee
Aaron Herting <aaron@herting.cc>
2014-05-01 12:48:41 -0400

    should be working...

--- sin_fe.rb
+++ sin_fe.rb
@@ -1,13 +1,18 @@
 #!/usr/bin/env ruby
 
 require 'sinatra'
+require_relative 'config.rb'
+require_relative 'lib/adsb.rb'
 
 
+$db = ADSB::Database.new
+$listener = ADSB::Listener.new $HOSTNAME, $PORT, $db
 
 
 
 get '/adsb.kml' do
-
+	content_type 'application/vnd.google-earth.kml+xml'
+	return $db.to_kml.render
 end
 
 
